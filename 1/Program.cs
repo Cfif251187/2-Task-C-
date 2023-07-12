901b5b31321ef94be8ffd8aca72c018b181c67f3
Sasha <efremovasasha@rambler.ru>
2023-07-12 23:59:03 +0300

    .\1\

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Программа принимает на вход трехзначное число и на выходе показывает вторую цифру.
+
+Console.Write("Ведите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 1000)
+
+{
+    Console.WriteLine("Вы ввели неверное число. Повторите попытку");
+    return;
+}
+
+int hundreds = number / 100;
+int decade = number / 10;
+int secondNum = decade - hundreds * 10;
+Console.WriteLine($"Второе число {secondNum}");
