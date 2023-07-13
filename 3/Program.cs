7b530487cee5e2dc3cd5227c5000e94bde29f5a8
Sasha <efremovasasha@rambler.ru>
2023-07-13 13:52:18 +0300

    .\3\

---
+++ 3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №15.Задача принимает номер дня недели и выдает выходной это день или нет.
+bool weekDay(int num)
+{
+    if(num == 6 || num == 7) 
+    {
+        Console.WriteLine("Выходной день");
+        return true;
+    }
+     else 
+     { Console.WriteLine(" Не выходной день");
+     return false;
+     }
+}
+Console.Write("Введите день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = weekDay(num);
+Console.WriteLine(result);
