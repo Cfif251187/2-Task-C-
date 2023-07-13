34df3f6e72f7d66d721ba4c78634c392be0bf874
Sasha <efremovasasha@rambler.ru>
2023-07-13 12:44:52 +0300

    .\2\

---
+++ 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Программа выводит третью цифру числа.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Третьего числа нет. Повторите попытку");
+    return;
+}
+
+if (number > 1000)
+{
+  int thirdNumber = number / 10 % 10;
+  Console.WriteLine($"Третье число {thirdNumber}");
+  return;
+}
+
+int thirdNum = number % 10;
+Console.WriteLine($"Третье число {thirdNum}");
